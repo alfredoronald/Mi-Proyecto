a2d4a72134eebe166aed71368cdc7cd649d396a2
Ronald <choquerivealfredoronald@gmail.com>
2024-07-15 02:01:51 -0300

    docs(web):change file button

---
+++ React/src/Components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Button.css'
+
+export function Button(props) {
+    const {children} = props
+  return (
+    <button>
+        {children}
+    </button>
+  )
+} 
