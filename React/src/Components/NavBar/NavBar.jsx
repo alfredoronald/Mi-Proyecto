0fff4a0a26a21a16e4e64128b6cc146d05d955e8
Ronald <choquerivealfredoronald@gmail.com>
2024-07-15 02:03:11 -0300

    docs(web):change file NavBar

---
+++ React/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <header className="header">
+        <a href="/" className="logo"> Sweet Dreams</a>
+
+        <nav className="navbar">
+            <a href="/">Inicio</a>
+            <a href="/">Productos</a>
+            <a href="/">Carrito</a>
+            <a href="/">Cuenta</a>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
