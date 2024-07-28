a9d63d2a9c8c32a2ee4e9ef00ae1e59220c99dcd
Ronald <choquerivealfredoronald@gmail.com>
2024-07-28 12:19:53 -0300

    docs(web ):changes files Roots

---
+++ React/src/routes/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import './Root.css'
+import NavBar from "../Components/NavBar/NavBar";
+
+
+export default function Root() {
+    return (
+      <>
+        <NavBar/>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </>
+    );
+  }
